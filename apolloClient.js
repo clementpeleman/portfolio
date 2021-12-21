67f2e780168bf42d13f3c1309ba891d010d8ef83
Clement Peleman <clementpeleman@outlook.com>
2021-12-21 15:35:07 +0100

    apolloclient

--- apolloClient.js
+++ apolloClient.js
@@ -1,11 +1,17 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+
 const client = new ApolloClient({
     uri: "https://api-eu-central-1.graphcms.com/v2/ckx0a5ow80od401xpen297noz/master",
     cache: new InMemoryCache(),
     defaultOptions: {
+      watchQuery: {
+        fetchPolicy: 'no-cache',
+        errorPolicy: 'ignore',
+      },
       query: {
-          fetchPolicy: 'no-cache',
+        fetchPolicy: 'no-cache',
+        errorPolicy: 'all',
       },
   }
 });
